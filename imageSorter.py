8fb9b998239d5cedd18e7e25062c94c6c9fa6b41
Mateusz Grzymowicz <mateusz.grzymowicz@mpicosys.com>
2019-06-11 09:21:57 +0200

    fixes to make it work on windows

--- imageSorter.py
+++ imageSorter.py
@@ -11,11 +11,18 @@ def sortPhotos():
             tags = exifread.process_file(f)
             for tag in tags.keys():
                 if tag in ('Image DateTime'):
+                    # checked_dir_year = str(tags[tag])[0:4]                                    for windows uncomment this
+                    # checked_dir_month = str(tags[tag])[5:7]                                   for windows uncomment this
+                    # checked_dir_no_semicolor = checked_dir_year + '-' + checked_dir_month     for windows uncomment this
                     checked_dir = str(tags[tag])[0:7]
-                    if not os.path.exists(checked_dir):
-                        os.mkdir(checked_dir)
+                    # if not os.path.exists(checked_dir_no_semicolor):                          for windows uncomment this
+                    #     os.mkdir(checked_dir_no_semicolor)                                    for windows uncomment this
+                    if not os.path.exists(checked_dir):                                         # for windows comment this
+                        os.mkdir(checked_dir)                                                   # for windows comment this
                     if checked_dir in str(tags[tag]):
-                        output_dir = './' + checked_dir + '/'
+                        # output_dir = './' + checked_dir_no_semicolor + '/'                    for windows uncomment this
+                        # f.close()                                                             for windows uncomment this
+                        output_dir = './' + checked_dir + '/'                                   # for windows comment this
                         shutil.move((input_dir + name), (output_dir + name))
 
 
